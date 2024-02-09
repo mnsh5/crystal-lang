c1dd98b381e3a82cee0cf434751aa5f0db79810b
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-09 17:22:33 -0300

    :rocket: add type inference

--- 03_type_inference.cr
+++ 03_type_inference.cr
@@ -1,7 +1,7 @@
 # Crystal necesita saber, de forma obvia (tan obvia como para un humano), los tipos de variables de instancia y clase
 
 # Con restricciones de tipo
-class Person
+class Person1
   @name : String
   @age : Int32
 
@@ -12,7 +12,7 @@ end
 
 # Sin restricciones de tipo
 # Asignar un valor literal
-class Person
+class Person2
   def initialize
     @name = "John Doe"
     @age = 0
@@ -20,8 +20,15 @@ class Person
 end
 
 # Asignar una variable que es un parámetro de método con una restricción de tipo
-class Person
+class Person3
   def initialize(name : String)
     @name = name
   end
+
+  def greet
+    "Hi #{@name}"
+  end
 end
+
+robin = Person3.new("Robin Hood")
+puts robin.greet
